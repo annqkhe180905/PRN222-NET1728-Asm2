d5a12d39ce55a6aa0f612e1e6c4680d821a837bd
Nguyen Hau <hau.dtnh@gmail.com>
2025-03-07 11:00:30 +0700

    add gitignore

--- FUNewsManagementSystem/BusinessLogicObject/obj/Debug/net8.0/BusinessLogicLayer.AssemblyInfo.cs
+++ FUNewsManagementSystem/BusinessLogicObject/obj/Debug/net8.0/BusinessLogicLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c1f1adf04f299c27819ba1754f81bf7f725dc38")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
