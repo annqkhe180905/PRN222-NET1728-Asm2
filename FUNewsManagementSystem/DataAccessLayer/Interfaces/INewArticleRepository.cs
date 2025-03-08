183eb3c1c30e67e0750638a69a49560bfbfc4159
NET_12_NguyenQuangKhanhAn <annqkhe180905@fpt.edu.vn>
2025-03-08 16:24:49 +0700

    feat: service, repo

--- FUNewsManagementSystem/DataAccessLayer/Interfaces/INewArticleRepository.cs
+++ FUNewsManagementSystem/DataAccessLayer/Interfaces/INewArticleRepository.cs
@@ -10,6 +10,15 @@ namespace DataAccessLayer.Interfaces
     public interface INewArticleRepository
     {
         Task<IEnumerable<NewsArticle>> GetAllActiveArticles();
+        Task<List<NewsArticle>> GetAllNewsAsync();
+        Task<NewsArticle?> GetByIdAsync(string newsArticleId);
+        Task AddAsync(NewsArticle newsArticle);
+        Task UpdateAsync(NewsArticle newsArticle);
+        Task DeleteAsync(string id);
+        Task<IEnumerable<NewsArticle>> SearchAsync(string keyword, short? categoryId, int[]? tagIds);
+        Task<IEnumerable<NewsArticle>> GetNewsHistoryByStaffIdAsync(short staffId);
+        Task<IEnumerable<NewsArticle>> GetActiveNewsForLecturersAsync();
+
 
     }
 }
