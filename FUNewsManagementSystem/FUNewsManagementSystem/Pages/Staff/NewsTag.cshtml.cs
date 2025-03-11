46a3786d15c6db74656fcb1366054f2abaca0841
Nguyen Hau <hau.dtnh@gmail.com>
2025-03-11 10:22:31 +0700

    fix: report ui

--- FUNewsManagementSystem/FUNewsManagementSystem/Pages/Staff/NewsTag.cshtml.cs
+++ FUNewsManagementSystem/FUNewsManagementSystem/Pages/Staff/NewsTag.cshtml.cs
@@ -1,11 +1,12 @@
 using BusinessLogicLayer.DTOs;
 using BusinessLogicLayer.Interfaces;
+using FUNewsManagementSystem.Helpers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FUNewsManagementSystem.Pages.Staff
 {
-    public class NewsTagModel : PageModel
+    public class NewsTagModel : BasePageModel
     {
         private readonly ITagServices _tagService;
         public List<TagDTO> Tags { get; set; } = new();
